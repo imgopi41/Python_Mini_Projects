b94bf0e59b9b3d8233b8c76cdb7745d232bf440d
Gopinath <99798157+imgopi41@users.noreply.github.com>
2022-03-02 13:45:59 +0530

    Add files via upload

---
+++ Email_Slicer.py
@@ -0,0 +1,5 @@
+email = input("Enter the Email-Id : ")  
+username = email[:email.index('@')] 
+domain = email[email.index('@')+1:] 
+result = "your username = {}\nyour domain = {}".format(username,domain)
+print(result)
