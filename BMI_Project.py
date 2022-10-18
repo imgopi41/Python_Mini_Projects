9795c87afafecfa884c37a72574659abf9ad15fe
Gopinath Pathakala <99798157+imgopi41@users.noreply.github.com>
2022-10-18 12:24:20 +0530

    Update BMI_Project.py

--- BMI_Project.py
+++ BMI_Project.py
@@ -1,11 +1,10 @@
-name = input("Enter the name: ")    #user name   
+name = input("\nEnter the name: ")    #user name   
 feet = float(input("Enter your height in feet: "))    #user height 
 Con_meter = feet/3.281  #converting user (height feet to meters)
 weight = float(input("Enter your weight: "))    #user weight
 Bmi = weight / (Con_meter ** 2) #BMI formula used to notify user is under weight or over weight.
 
 if Bmi <=25: # checking BMI is below or equal to '25'.
-    print("Congratulations {} your are under weight by {} is your body mass index".format(name,Bmi))   #under-weight message
+    print("Congratulations {} your are under weight.\nThis is your body mass index {}".format(name,Bmi))   #under-weight message
 else:
-    print("oops..!{} your are over weight by {} is your body mass index".format(name,Bmi))  #over-weight message
-
+    print("oops..! your are over weight {}.\nThis is your body mass index {}".format(name,Bmi))  #over-weight message
